6835ac9cd8c5b59ef1a95d706867ddbfba15fe49
raildom <raildom05@gmail.com>
2025-09-17 13:26:00 -0300

    correções na segunda questão

--- 5 periodo/Monitoria de Algoritmos II/Atividade em sala (19_09_2025)/q02.c
+++ 5 periodo/Monitoria de Algoritmos II/Atividade em sala (19_09_2025)/q02.c
@@ -2,27 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-int LancarDados(){
+int lancar_dados(){
     return rand() % 6 + 1;
 }
 
-void MensagemResultado(int primeiro, int segundo, int soma){
-    if(soma == 7 || soma == 11){
-        printf("A soma dos dados %d + %d = %d \nO Jogador ganhou :)\n", primeiro, segundo, soma);
-    }else if(soma == 2 || soma == 3 || soma == 12){
-        printf("A soma dos dados %d + %d = %d \nO Jogador perdeu :(\n", primeiro, segundo, soma);
-    }else if(soma == 4 || soma == 5 || soma == 6 || soma == 8 || soma == 9 || soma == 10){
-        printf("A soma dos dados %d + %d = %d \n\nO ponto e %d \n\n", primeiro, segundo, soma, soma);
-        JogoPonto(soma);
-    }
-}
-
-void JogoPonto(int ponto){
+void outros_lancamentos(int ponto){
     int primeiro, segundo, auxiliar;
 
     while(1){
-        primeiro = LancarDados();
-        segundo = LancarDados();
+        primeiro = lancar_dados();
+        segundo = lancar_dados();
         auxiliar = primeiro + segundo;
 
         printf("A soma dos dados %d + %d = %d \n", primeiro, segundo, auxiliar);
@@ -36,15 +25,26 @@ void JogoPonto(int ponto){
     }
 }
 
+void primeiro_lancamento(int primeiro, int segundo, int soma){
+    if(soma == 7 || soma == 11){
+        printf("A soma dos dados %d + %d = %d \nO Jogador ganhou :)\n", primeiro, segundo, soma);
+    }else if(soma == 2 || soma == 3 || soma == 12){
+        printf("A soma dos dados %d + %d = %d \nO Jogador perdeu :(\n", primeiro, segundo, soma);
+    }else if(soma == 4 || soma == 5 || soma == 6 || soma == 8 || soma == 9 || soma == 10){
+        printf("A soma dos dados %d + %d = %d \n\nO ponto e %d \n\n", primeiro, segundo, soma, soma);
+        outros_lancamentos(soma);
+    }
+}
+
 int main(){
     int primeiro, segundo, soma;
 
     srand(time(NULL));
 
-    primeiro = LancarDados();
-    segundo = LancarDados();
+    primeiro = lancar_dados();
+    segundo = lancar_dados();
     soma = primeiro + segundo;
 
-    MensagemResultado(primeiro, segundo, soma);
+    primeiro_lancamento(primeiro, segundo, soma);
     return 0;
 }
