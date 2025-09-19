93d32446cb40ce983daa58d8d1d7bb7852bde492
raildom <raildom05@gmail.com>
2025-09-18 21:06:12 -0300

    correção da primeira questão (monitoria presencial)

--- 5 periodo/Monitoria de Algoritmos II/Atividade em sala (19_09_2025)/q01.c
+++ 5 periodo/Monitoria de Algoritmos II/Atividade em sala (19_09_2025)/q01.c
@@ -1,28 +1,49 @@
 #include <stdio.h>
 
-int ehPrimo(int n){
-    int primo = 1;
-
-    if(n <= 1)
-        primo = 0;
-    else{
-        for(int i = 2; i <= n / 2; i++){
-            if(n % i == 0)
-                primo = 0;
+void preencher_matriz(int linhas, int colunas, int matriz[linhas][colunas]){
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            printf("Digite o valor para a posicao [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
         }
     }
-    return primo;
+}
+
+int eh_esparsa(int linhas, int colunas, int matriz[linhas][colunas]){
+    int quant_zeros = 0, esparsa = 1;
+
+    for(int i = 0; i < linhas; i++){
+        for(int j = 0; j < colunas; j++){
+            if(matriz[i][j] == 0){
+                quant_zeros++;
+            }
+        }
+    }
+
+    float proporcao = (float)quant_zeros / (linhas * colunas) * 100;
+    if(proporcao <= 50){
+        esparsa = 0; // Matriz não esparsa
+    }
+    printf("Proporcao de zeros: %.2f%%\n", proporcao * 100);
+    return esparsa;
 }
 
 int main(){
-    int n;
+    int linhas, colunas;
 
-    printf("Digite um numero inteiro: ");
-    scanf("%d", &n);
+    printf("Digite o numero de linhas: ");
+    scanf("%d", &linhas);
+    printf("Digite o numero de colunas: ");
+    scanf("%d", &colunas);
 
-    if(ehPrimo(n) == 1)
-        printf("O numero %d e primo.\n", n);
-    else
-        printf("O numero %d nao e primo.\n", n);
+    int matriz[linhas][colunas];
+
+    preencher_matriz(linhas, colunas, matriz);
+
+    if(eh_esparsa(linhas, colunas, matriz)){
+        printf("A matriz e esparsa.\n");
+    }else{
+        printf("A matriz nao e esparsa.\n");
+    }
     return 0;
 }
